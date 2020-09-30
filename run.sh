777bb0a6377143d741f7c73c98414a8589eb871f
dmtkachenko <42244720+dmtkachenko@users.noreply.github.com>
2020-09-30 12:03:53 +0300

    - Dockerfile testing

--- run.sh
+++ run.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 #
+#
+#JAVA_OPTS=$JAVA_OPTIONS
+#
 DB_URL="jdbc:postgresql://${DB_HOST}:${DB_PORT}/${DB_DATABASE}"
-JAVA_OPTS="${JAVA_OPTIONS} \
-        -Dspring.datasource.username=${DB_USERNAME} \
-        -Dspring.datasource.password=${DB_PASSWORD}"
-#        -Dspring.datasource.url=${DB_URL}"
 #
-echo "JAVA_OPTS=${JAVA_OPTS}"
+APP_OPTS="--spring.datasource.username=${DB_USERNAME} \
+        --spring.datasource.password=${DB_PASSWORD} \
+        --spring.datasource.url=${DB_URL}"
 #
-#exec "java ${JAVA_OPTS} org.springframework.boot.loader.JarLauncher"
+#
+echo "APP_OPTS=${APP_OPTS}"
+#
+exec java org.springframework.boot.loader.JarLauncher ${APP_OPTS}
