b7945f70b7622687c993ffdad4cc17512a427865
Yusuf Murodov <160738549+yusuf-murodov-epam@users.noreply.github.com>
2025-01-17 18:46:39 +0500

    defect/DE97327 Fix - uses only validation for password against username

--- src/main/java/org/folio/pv/service/validator/RegExpValidator.java
+++ src/main/java/org/folio/pv/service/validator/RegExpValidator.java
@@ -3,6 +3,7 @@ package org.folio.pv.service.validator;
 import static org.apache.commons.lang3.StringUtils.isNotBlank;
 
 import java.util.regex.Pattern;
+
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -22,18 +23,20 @@ public class RegExpValidator implements Validator {
   @Override
   public ValidationErrors validate(String password, UserData user) {
     var expression = rule.getRuleExpression();
-
+    var userName = user.getName();
     var failed = false;
     if (isNotBlank(expression)) {
-      var passwordWithoutSpaces = password.replaceAll("\\s", "");
-      var usernameWithoutSpaces = user.getName().replaceAll("\\s", "");
+      if (expression.contains(REGEXP_USER_NAME_PLACEHOLDER)) {
+        password = password.replaceAll("\\s", "");
+        userName = userName.replaceAll("\\s", "");
+      }
 
-      var exprWithUser = expression.replace(REGEXP_USER_NAME_PLACEHOLDER, usernameWithoutSpaces);
+      var exprWithUser = expression.replace(REGEXP_USER_NAME_PLACEHOLDER, userName);
       log.info("Validating password against regexp: {}", exprWithUser);
 
       var pattern = Pattern.compile(exprWithUser);
 
-      failed = !pattern.matcher(passwordWithoutSpaces).matches();
+      failed = !pattern.matcher(password).matches();
     }
 
     if (failed) {
